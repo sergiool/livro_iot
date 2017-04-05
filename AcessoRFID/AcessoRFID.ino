71f2b4ae5b667123f68ffb0906a699ac920a74c8
Sergio de Oliveira <sergiool@ufsj.edu.br>
2017-04-04 21:34:59 -0300

    RFID

--- AcessoRFID/AcessoRFID.ino
+++ AcessoRFID/AcessoRFID.ino
@@ -1,6 +1,7 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <SPI.h>
+#include <FS.h>
 #include <MFRC522.h>
 
 /* Ligações do MFRC522 para o ESP8266 (ESP-12)
@@ -36,6 +37,28 @@ void setup() {
     Serial.println("WiFi conectado");
 }
 
+
+void atualiza_local(){
+    HTTPClient http;
+    Serial.print("[HTTP] Início...\n");
+    // Configura URL
+    http.begin("http://sergiool.esy.es/list.php");
+    if(http.GET() == HTTP_CODE_OK) { // Conectou, atualiza o arquivo
+        String remoto = http.getString();
+        Serial.printf("Remoto: %s\n", remoto.c_str());
+        File f = SPIFFS.open("/f.txt", "r");
+        String local = f.readString();
+        Serial.printf("Local: %s\n", local.c_str());
+        f.close();
+        if (remoto != local){
+          Serial.println("Atualizando ...");
+          File f = SPIFFS.open("/f.txt", "w");
+          f.print(remoto);
+        }
+    }
+}
+
+
 char * read_RFID(char *buffer){ // Função para ler as tags
     // Verifica se há cartões presentes e Lê um cartão
     if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
@@ -58,38 +81,58 @@ char * dump_byte_array(byte *buffer, byte bufferSize, char * result) {
     return result;
 }
 
+unsigned long anterior_tarefa = 0;
+
 void loop() {
   char code_RFID[20] = "";
+  int httpCode = 0;
+
+  if ((millis() - anterior_tarefa > 100000) && (WiFi.status() == WL_CONNECTED)) {
+      atualiza_local();
+      anterior_tarefa = millis();
+  }
+
   if (read_RFID(code_RFID)){ // Verificar se o cartão foi lido
 
      if(WiFi.status() == WL_CONNECTED) { // Verifica a conexão Wifi
-       HTTPClient http;
-       Serial.print("[HTTP] Início...\n");
-       // Configura URL, cabeçalhos e monta o método POST com o código da TAG
-       http.begin("http://sergiool.esy.es/consult.php");
-       http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-       int httpCode = http.POST(String("tag=") + code_RFID);
+
+      HTTPClient http;
+      Serial.print("[HTTP] Início...\n");
+      // Configura URL, define cabeçalhos e monta o método POST com a TAG
+      http.begin("http://sergiool.esy.es/consult.php");
+      http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+      httpCode = http.POST(String("tag=") + code_RFID);
 
         // httpCode será negativo em caso de erro
-       if(httpCode > 0) {
+      if(httpCode > 0) {
           // Obteve resposta do servidor
-         Serial.printf("[HTTP] POST... código: %d\n", httpCode);
+         Serial.printf("[HTTP] POST ... código: %d\n", httpCode);
 
           // Achou o arquivo
-         if(httpCode == HTTP_CODE_OK) { // Conectou, atualiza o arquivo
-           String payload = http.getString();
-           Serial.println(payload);
-           if (payload == "OK"){
-             digitalWrite(D0, HIGH);
-             delay(100); // Tempo suficiente para acionar o acesso
-             digitalWrite(D0, LOW);
-           }
-         }
-       } else
-         Serial.printf("POST Erro: %s\n", http.errorToString(httpCode).c_str());
-       http.end();
-       delay(300); // Tempo suficiente para acionar o acesso
-
+        if(httpCode == HTTP_CODE_OK) { // Conectou, atualiza o arquivo
+          String payload = http.getString();
+          Serial.println(payload);
+          if (payload == "OK"){ // Localizou, libera o acesso
+            digitalWrite(D0, HIGH);
+            delay(100); // Tempo suficiente para acionar o acesso
+            digitalWrite(D0, LOW);
+          }
+        }
+      } else
+        Serial.printf("[HTTP] POST... código: %d\n", httpCode);
+	http.end();
+    }
+    if ((WiFi.status() != WL_CONNECTED) || (httpCode != HTTP_CODE_OK)){
+      // Houve erro na consulta ao banco
+      File f = SPIFFS.open("/f.txt", "r");
+      String local = f.readString();
+      f.close();
+      if (local.indexOf(code_RFID) > 0){ // Localizou no arquivo
+        Serial.printf("Acesso local liberado: %s\n", local.c_str());
+        digitalWrite(D0, HIGH);
+        delay(100); // Tempo suficiente para acionar o acesso
+        digitalWrite(D0, LOW);
+      }
     }
   }
 }
