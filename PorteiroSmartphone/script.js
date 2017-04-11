c50af3bb83398093a25fb4c0a543c0b433506fc1
Sergio de Oliveira <sergiool@ufsj.edu.br>
2017-04-10 22:25:44 -0300

    Porteiro Smartphone

---
+++ PorteiroSmartphone/script.js
@@ -0,0 +1,14 @@
+﻿$(function(){
+  var client = mqtt.connect(‘mqtt://cap_iot_:iotiot256@broker.shiftr.io’, {
+    clientId: ‘javascript’
+  });
+
+  client.on(‘connect’, function(){
+    console.log(‘Cliente conectado!’);
+  });
+
+  $(‘#button’).click(function(){
+   client.publish(‘/PorteiroEletronico’, 'Abre');
+      window.alert('Comando enviado');	
+  })
+})
